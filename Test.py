538046d49d5365efa27b2b01ebc87a77ee4f895b
takkzapo <113465413+takkzapo@users.noreply.github.com>
2022-09-30 02:01:01 +0700

    Add files via upload

---
+++ Test.py
@@ -0,0 +1,27 @@
+from colorama import Fore, Back, Style
+import socket
+import os
+import requests
+import random
+import getpass
+import time
+import sys
+
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+proxys = open('proxies.txt').readlines()
+bots = len(proxys)
+
+def si():
+    print("Zalo/Call: 0782554949")
+    print("Information: https://leminhtus.info")
+
+def menu():
+    sys.stdout.write(f"BANI DDoS Update 0.2.9")
+    clear()
+    print('BANI DDOS By LeMinhTu [LeMinhTu.info] ')
+    print("https://LeMinhTu.info")
+    print(Fore.YELLOW +"╔╗ ╔═╗" + Fore.RED +"╔╗╔╦")
+    print(Fore.YELLOW +"╠╩╗╠═╣" + Fore.RED +"║║║║")
+    print(Fore.YELLOW +"╚═╝╩ ╩" + Fore.RED +"╝╚╝╩")
