04c2b12519ca64e880d3eb938ae69e2e22471d39
takkzapo <113465413+takkzapo@users.noreply.github.com>
2022-09-29 19:17:10 +0000

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,14 +1,8 @@
 from colorama import Fore, Back, Style
-
-def si():
-    print("Zalo/Call: 0782554949")
-    print("Information: https://leminhtus.info")
-
-def menu():
-    sys.stdout.write(f"BANI DDoS Update 0.2.9")
-    clear()
-    print('BANI DDOS By LeMinhTu [LeMinhTu.info] ')
-    print("https://LeMinhTu.info")
-    print(Fore.YELLOW +"╔╗ ╔═╗" + Fore.RED +"╔╗╔╦")
-    print(Fore.YELLOW +"╠╩╗╠═╣" + Fore.RED +"║║║║")
-    print(Fore.YELLOW +"╚═╝╩ ╩" + Fore.RED +"╝╚╝╩")
+print("Zalo/Call: 0782554949")
+print("Information: https://leminhtus.info")
+print('BANI DDOS By LeMinhTu [LeMinhTu.info] ')
+print("https://LeMinhTu.info")
+print(Fore.YELLOW +"╔╗ ╔═╗" + Fore.RED +"╔╗╔╦")
+print(Fore.YELLOW +"╠╩╗╠═╣" + Fore.RED +"║║║║")
+print(Fore.YELLOW +"╚═╝╩ ╩" + Fore.RED +"╝╚╝╩")
